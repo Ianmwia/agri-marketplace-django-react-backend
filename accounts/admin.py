21817f290d6d9703e1c36b704e5e829cf3e61c00
Mwia <iansnr10@gmail.com>
2026-02-04 09:54:06 +0300

    creating the user roles and renaming CustomUser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Customuser
+from .models import CustomUser
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
-    model = Customuser
+    model = CustomUser
     list_display = ['email', 'first_name', 'last_name']
     search_fields = ['email']
     ordering = ('email',)
-admin.site.register(Customuser, CustomUserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -17,27 +17,35 @@ class CustomUserManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
         return user
     
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')
 
         return self.create_user(email, password, **extra_fields)
     
-class Customuser(AbstractBaseUser, PermissionsMixin):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     '''
     Customuser to extend baseUser manager for authentication and permissions
     '''
+    ROLES = (
+        ('farmer', 'Farmer'),
+        ('buyer','Buyer'),
+        ('admin','Admin'),
+        ('agrivet','AgriVet'),
+    )
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     email = models.EmailField(_("email address"), unique=True)
+    role = models.CharField(max_length=30, choices=ROLES)
     phone = models.CharField(_("phone number"), max_length=90)
     location = models.CharField(_("location"), max_length=100)
     date_joined = models.DateTimeField(_("date joined"), auto_now_add=True)
 
-    is_active = models.BooleanField(_(""), default=True)
+    is_active = models.BooleanField(_(""), default=False)
     is_staff = models.BooleanField(_(""), default=True)
 
     USERNAME_FIELD = 'email'
