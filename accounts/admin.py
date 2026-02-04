4f1d5309df163c5a1c6a747ba89773ab39513b65
Mwia <iansnr10@gmail.com>
2026-02-04 14:20:51 +0300

    create user fields to add in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,25 @@ from .models import CustomUser
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['email', 'first_name', 'last_name']
-    search_fields = ['email']
+    list_display = ['email', 'first_name', 'last_name', 'role', 'is_active', 'is_staff']
+    search_fields = ['email', 'first_name', 'last_name']
     ordering = ('email',)
+
+    #field shown when editing an existing user
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Info', {'fields': ('first_name', 'last_name', 'role', 'location')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    #field shown when creating a new user
+    add_fieldsets = (
+        (None, {'classes': ('wide',),
+                'fields': ('email','password1', 'password2','first_name', 'last_name', 
+                           'role', 'location',
+                           'is_active', 'is_staff', 'is_superuser'
+                        ),
+                }),
+        )
+
+
 admin.site.register(CustomUser, CustomUserAdmin)
