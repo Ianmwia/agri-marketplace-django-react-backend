fda9ab5439d97188c9be29274d7018c81aee809e
Mwia <iansnr10@gmail.com>
2026-02-04 14:10:13 +0300

    accept input by name not id in fields with foreigh key

--- produce/serializers.py
+++ produce/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Category, Produce
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,6 +10,14 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 class ProduceSerializer(serializers.ModelSerializer):
+    #accept input by email not fk id
+    farmer = serializers.SlugRelatedField(
+        slug_field='email',
+        queryset=User.objects.all(),
+        required = False
+    )
+
+    category = serializers.CharField()
     class Meta:
         model = Produce
         fields = '__all__'
@@ -15,5 +26,13 @@ class ProduceSerializer(serializers.ModelSerializer):
         user = self.context['request'].user
         if user.role != 'farmer':
             raise serializers.ValidationError('only farmers can create produce')
+        
+        #accept input by name not fk id
+        category_name  = self.initial_data.get('category')
+
+        if category_name:
+            category_object,_ = Category.objects.get_or_create(name=category_name)
+            validated_data['category'] = category_object
+
         validated_data['farmer'] = user
         return super().create(validated_data)
