aefa71f8f84d3a0c597c601ba74c20bdf40a3afd
Mwia <iansnr10@gmail.com>
2026-02-04 10:44:53 +0300

    creating the register serializer and register view viewset

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .serializers import RegisterSerializer
+from .models import CustomUser
+from rest_framework.response import Response
+from rest_framework import viewsets
 
 # Create your views here.
+#class based Api Views
+class RegisterViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    
