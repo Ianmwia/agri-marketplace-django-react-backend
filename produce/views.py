f341a77e52fad2931c99f2740f333e9d2140e762
Mwia <iansnr10@gmail.com>
2026-02-08 14:35:40 +0300

    adding permissions to produce so that only farmers can check and make produce listings

--- produce/views.py
+++ produce/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, serializers
 from .models import Produce
 from .serializers import ProduceSerializer
 
 # Create your views here.
+
+class IsAFarmer(permissions.BasePermission):
+    '''check if the authenticated user role is farmer'''
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'farmer'
+    
 class ProduceViewSet(viewsets.ModelViewSet):
-    queryset = Produce.objects.all().order_by('-date_created')
     serializer_class = ProduceSerializer
+    permission_classes = [IsAFarmer]
+
+    def get_queryset(self):
+        '''list all each farmers produce'''
+        return Produce.objects.filter(farmer=self.request.user).order_by('-date_created')
+    
+    def perform_create(self, serializer):
+        serializer.save(farmer=self.request.user)
+    
