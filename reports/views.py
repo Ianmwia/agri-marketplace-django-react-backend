b26c7a2cc98b0b929bb42b359898df18a617415b
Mwia <iansnr10@gmail.com>
2026-02-10 13:03:52 +0300

    create the template rendering for reports app field officers

--- reports/views.py
+++ reports/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Report
 from .serializers import ReportSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import TemplateHTMLRenderer
+
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 
 # Create your views here.
 class ReportViewSet(viewsets.ModelViewSet):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    #http render in django
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'reports.html'
+
+    def get(self, request):
+        queryset = self.get_queryset()
+        return Response({'reports': queryset})
     
