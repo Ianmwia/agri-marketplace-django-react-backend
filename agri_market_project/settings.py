c74143568eb9ba0a2dbb7400d77260816401e338
Mwia <iansnr10@gmail.com>
2026-02-20 13:30:06 +0300

    added cors with localhost and lax sessions for cookie persist

--- agri_market_project/settings.py
+++ agri_market_project/settings.py
@@ -54,9 +54,12 @@ INSTALLED_APPS = [
     'reports',
     'drf_yasg',
     'services',
+    'django_extensions',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -116,6 +119,21 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+#react integration 180226 18.15
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+#trust the origin for csrf protection
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:5173"
+]
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
@@ -126,6 +144,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
 
 # to auto add slashes to say there is more after
 APPEND_SLASH = True
@@ -151,3 +170,14 @@ LOGOUT_REDIRECT_URL = 'login-list'
 SWAGGER_SETTINGS = {
     'LOGOUT_URL' : '/logout/',
 }
+
+
+#Sessions auth lax
+SESSION_COOKIE_SAMESITE = 'Lax'
+CSRF_COOKIE_SAMESITE = 'Lax'
+
+
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+
+SESSION_COOKIE_HISTORY = True
