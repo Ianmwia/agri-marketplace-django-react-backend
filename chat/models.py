95d6eef86b94417f8d494c0d5fa23525d9a55aa6
Mwia <iansnr10@gmail.com>
2026-03-01 09:36:43 +0300

    updating model to make sure thread duplicates do not exist to clash with the database

--- chat/models.py
+++ chat/models.py
@@ -4,20 +4,30 @@ from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Thread(models.Model):
+    ''' Container for thread, so keep 2 users in conversation'''
     user1 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='threads_user1')
     user2 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='threads_user2')
     updated_at = models.DateTimeField(auto_now=True)
 
-    constraints = [
-        models.UniqueConstraint(
-            fields = ['user1', 'user2'],
-            name='unique_thread'
-        )
-    ]
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields = ['user1', 'user2'],
+                name='unique_thread'
+            )
+        ]
 
     def clean(self):
+        #prevent self messaging
         if self.user1 == self.user2:
             raise ValidationError('Users cannot message themselves')
+        
+        #ensure user1 and user2 prevent duplicate containers
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        if self.user1_id > self.user2_id:
+            self.user1, self.user2 = self.user2, self.user1
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.user1.role} & {self.user2.role} Chat'
