c828909eeef1820981e60ad1d41dc5000513f099
Mwia <iansnr10@gmail.com>
2026-03-03 09:18:51 +0300

    updating orders model to include mpesa checkout id

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from produce.models import Produce
+from produce.models import Produce, ProduceBatch
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
@@ -14,20 +14,25 @@ class Order(models.Model):
         ('pending','Pending'),
         ('accepted','Accepted'),
         ('rejected','Rejected'),
+        ('paid', 'Paid'),
         ('delivered','Delivered')
     )
-    buyer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders')
-    produce = models.ForeignKey(Produce, on_delete=models.CASCADE, related_name='orders')
+    buyer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='buyer_orders')
+    batch = models.ForeignKey(ProduceBatch, related_name='orders', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(_("number of items ordered"))
     status = models.CharField(_("purchase status"), max_length=50, choices=STATUS, default='pending')
-    created_at = models.DateTimeField(_("date when produce was ordered"), auto_now_add=True)
 
+    #mpesa financials
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+    mpesa_checkout_id = models.CharField(max_length=50, blank=True, null=True)
+    created_at = models.DateTimeField(_("date when produce was ordered"), auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     #add rejection reason
     rejection_reason = models.TextField(_("reason for rejection"), blank=True, null=True)
 
-    @property
-    def total_amount(self):
-        return self.produce.price * self.quantity
+    def save(self, *args, **kwargs):
+        self.total_price = self.batch.price_per_unit * self.quantity
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'Order #{self.id} - {self.produce.name}'
+        return f'Order #{self.id} - {self.batch.produce.name}'
