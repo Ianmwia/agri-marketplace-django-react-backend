960101bb809e679e01604f68b803f314513ea507
Mwia <iansnr10@gmail.com>
2026-03-09 09:39:55 +0300

    create send sms fucntion to send messages to buyers from textbee

--- accounts/helpers.py
+++ accounts/helpers.py
@@ -1,3 +1,6 @@
+import requests
+from decouple import config
+
 def normalize_phone_number(phone):
     phone = phone.strip()
     if phone.startswith('+'):
@@ -7,3 +10,24 @@ def normalize_phone_number(phone):
     elif not phone.startswith('254'):
         phone = '254' + phone
     return phone
+
+#text bee send sms
+def send_free_sms(phone, message):
+    clean_phone = normalize_phone_number(phone)
+
+    BASE_URL = 'https://api.textbee.dev/api/v1'
+    API_KEY = config("TEXTBEE_API_KEY")
+    DEVICE_ID = config('TEXTBEE_DEVICE_ID')
+
+    response = requests.post(
+    f'{BASE_URL}/gateway/devices/{DEVICE_ID}/send-sms',
+    json={
+        'recipients': [clean_phone],
+        'message': message
+    },
+    headers={'x-api-key': API_KEY}
+    )
+    print(f'SMS Response Status: {response.status_code}')
+    print(f'SMS Response Body: {response.text}')
+
+    print(response.json())
