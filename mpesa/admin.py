576c3d62b5c7d49b90b9d658d6d97534edab4c46
Mwia <iansnr10@gmail.com>
2026-03-25 16:02:36 +0300

    register mpesa and stripe models and migrations

--- mpesa/admin.py
+++ mpesa/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MpesaCallBack, MpesaRequest, MpesaResponse, StripePayment
 
 # Register your models here.
+admin.site.register(MpesaCallBack)
+admin.site.register(MpesaResponse)
+admin.site.register(MpesaRequest)
+admin.site.register(StripePayment)
