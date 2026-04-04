15eefe8ecc3e17c68954662c3282890d2b81ded0
Mwia <iansnr10@gmail.com>
2026-04-04 14:36:08 +0300

    update smtp to use sendgrid

--- agri_market_project/settings.py
+++ agri_market_project/settings.py
@@ -385,9 +385,11 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 465
-    EMAIL_USE_TLS = True
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
     EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+    DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 #stripe
 STRIPE_PUBLIC_KEY=config('PK_TEST')
