effe2e0371e07e8b85a4267073baa38d2d5a545a
Mwia <iansnr10@gmail.com>
2026-04-04 20:40:30 +0300

    add accounts adapters for frontend site

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -4,3 +4,14 @@ from django.conf import settings
 class CustomAccountAdapter(DefaultAccountAdapter):
     def get_password_reset_url(self, request, user):
         return f'{settings.FRONTEND_URL}/reset-password/'
+
+
+from dj_rest_auth.serializers import PasswordResetSerializer
+class CustomPasswordSerializer(PasswordResetSerializer):
+    def get_email_options(self):
+        return {
+            'extra_email_context':{
+                'password_reset_url' : f'{settings.FRONTEND_URL}/reset-password/'
+            }
+        }
+        
